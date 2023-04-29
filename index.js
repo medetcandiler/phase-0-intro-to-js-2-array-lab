5408bc4461e1d6bcb106da3b8d83f7dac4848ee1
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-29 13:06:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+  cats.push(name);
+}
+
+// destructivelyAppendCat('Ralph');
+
+
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+}
+// destructivelyPrependCat('Bob');
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+}
+
+// destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+// destructivelyRemoveFirstCat();
+
+function appendCat(name){
+  const newCats = [...cats];
+  newCats.push(name);
+  console.log(newCats);
+  return newCats
+}
+
+appendCat('Broom');
+
+function prependCat(name){
+  const newCats = [...cats];
+  newCats.unshift(name);
+  return newCats;
+}
+
+prependCat('Arnold')
+
+function removeLastCat(){
+  const newCats = [...cats];
+  newCats.pop();
+  return newCats
+}
+
+function removeFirstCat(){
+  const newCats = [...cats];
+  newCats.shift();
+  return newCats;
+}
